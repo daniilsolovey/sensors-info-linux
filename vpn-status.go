509082cf743b1035bdefa0679e20979ae180519d
Daniil Solovey <daniil.soloveiy@gmail.com>
2026-08-24 01:03:24 +0600

    vpn connection status

--- vpn-status.go
+++ vpn-status.go
@@ -6,16 +6,23 @@ import (
 )
 
 func getCommonVPNStatus() (string, error) {
-	out, err := exec.Command("/bin/sh", "-c", "nmcli con show --active").Output()
-	exec.Command("/bin/sh", "-c", "sudo find ...")
+	out, err := exec.Command(
+		"nmcli",
+		"-t",
+		"-f", "TYPE",
+		"con", "show",
+		"--active",
+	).Output()
 	if err != nil {
 		return "", err
 	}
 
-	if strings.Contains(string(out), "tun0") {
-		return "Connected", nil
-	} else {
-		return "Disconnected", nil
+	for _, connType := range strings.Split(strings.TrimSpace(string(out)), "\n") {
+		switch connType {
+		case "vpn", "wireguard", "tun":
+			return "Connected", nil
+		}
 	}
 
+	return "Disconnected", nil
 }
